fd062471ca77c6a09ce614226841a7a8e44124b6
serkomut <serkomut@gmail.com>
2015-06-10 10:07:31 +0300

    profile model schema edited

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,29 +1,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-module.exports = mongoose.model('Profile', new Schema({
-	userId: String,
-	firstName: String,
-	lastName : String,
-	birthDay: String,
-	pictures: [Pictures],
-	adress: Adress
-}));
-
-var Adress = new Schema({
-	street: String,
-	streetNumber:String,
-	zipCode: String,
-	cityName: String,
-	countryCode: String,
-	geoCode:{
-		latitude:String,
-		longtitude:String
-	}
-});
+var Adress = new Schema();
 
 var Pictures = new Schema({
 	isDefault : Boolean,
 	imageUrl: String,
 	size: Number
 });
+
+module.exports = mongoose.model('Profile', new Schema({
+	userId: String,
+	firstName: String,
+	lastName : String,
+	birthDay: String,
+	pictures: [Pictures],
+	adress: {
+		street: String,
+		streetNumber:String,
+		zipCode: String,
+		cityName: String,
+		countryCode: String,
+		geoCode:{
+			latitude:String,
+			longtitude:String
+		}
+	}
+}));
+
