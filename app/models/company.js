d7cae2a095c7223f5e1bf35073dea93c16f5d70d
SEROL GUZEL <serol.guzel@paximum.com>
2015-07-01 17:40:49 +0300

    copany model created

---
+++ app/models/company.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('Companies', new Schema({
+    ownerId: String,
+    name: String,
+    logo: String,
+    code: String,
+    bankAccount: BankAccount,
+    adress: Adress,
+    emails : Email
+}));
+
+
+var Adress = {
+    street: String,
+    streetNumber: String,
+    zipCode: String,
+    cityName: String,
+    countryCode: String,
+    geoCode: {
+        latitude: String,
+        longtitude: String
+    }
+};
+
+var Email = {
+    mail: String,
+    IsDefault : Boolean
+};
+
+var BankAccount = {
+    bankName: String,
+    branchCode: String,
+    location: String,
+    accountNumber: String,
+    ibanNumber: String,
+    swiftCode: String,
+    currency: String
+};
